a96d76c9f2228af2fab917a2adee30b6184e2aa0
SvetlanaBolshova <80790487+SvetlanaBolshova@users.noreply.github.com>
2021-11-20 22:10:59 +0300

    Add files via upload

---
+++ les5/Category.java
@@ -0,0 +1,19 @@
+package les5;
+
+
+import lombok.Getter;
+
+@Getter
+public enum Category {
+
+    FOOD(1, "Food"),
+    ELECTRONIC(2, "Electronic");
+
+    private final long id;
+    private final String title;
+
+    Category(long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
